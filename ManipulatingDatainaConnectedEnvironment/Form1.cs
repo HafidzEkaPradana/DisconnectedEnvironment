3d1d4c132eee3d7d98a5138de333254d8b6becdc
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-06-09 13:20:12 +0700

    Add project files.

---
+++ ManipulatingDatainaConnectedEnvironment/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipulatingDatainaConnectedEnvironment
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        DataRow dr;
+        string code;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hRDataSet.empdetails' table. You can move, or remove it, as needed.
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+            txCode.Enabled = false;
+            txName.Enabled = false;
+
+
+        }
+    }
+}
